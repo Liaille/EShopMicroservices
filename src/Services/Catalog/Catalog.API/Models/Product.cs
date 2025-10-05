9337a7297f158d577227e217dc87731ea3c1f855
Li <820208235@qq.com>
2025-10-05 15:51:10 +0800

    新增产品实体

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿namespace Catalog.API.Models;
+
+/// <summary>
+/// 产品
+/// </summary>
+public class Product
+{
+    /// <summary>
+    /// 主键
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// 名称
+    /// </summary>
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// 类别列表
+    /// </summary>
+    public List<string> Category { get; set; } = [];
+
+    /// <summary>
+    /// 描述
+    /// </summary>
+    public string Description { get; set; } = default!;
+
+    /// <summary>
+    /// 图片文件
+    /// </summary>
+    public string ImageFile { get; set; } = default!;
+
+    /// <summary>
+    /// 价格
+    /// </summary>
+    public decimal Price { get; set; }
+}

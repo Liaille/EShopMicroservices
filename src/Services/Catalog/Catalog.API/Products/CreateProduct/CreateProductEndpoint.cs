ef3a35bba81a0dd9cff0e6e09710b92966b4dfd6
Li <820208235@qq.com>
2025-10-05 19:27:05 +0800

    添加Mapster和Carter引用

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -17,6 +17,21 @@ public record CreateProductResponse(Guid Id);
 /// <summary>
 /// 应用程序API层
 /// </summary>
-public class CreateProductEndpoint
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateProductResponse>();
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct") // 指定路由名称
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created) // 指定成功响应类型和状态码
+        .ProducesProblem(StatusCodes.Status400BadRequest) // 指定可能的错误响应
+        .WithSummary("Creates a new product") // 添加摘要
+        .WithDescription("Creates a new product with the provided details."); // 添加描述
+    }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,15 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
+// 向容器中添加服务。
+builder.Services.AddCarter(); // 一个为ASP.NET Core最小API提供拓展功能和模块化的轻量级框架
+builder.Services.AddMediatR(config =>
+{
+    // 将此项目中的所有服务都注册到中介类库中
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.MapGet("/", () => "Hello World!");
+// 配置HTTP请求管道。
+app.MapCarter();
 
 app.Run();

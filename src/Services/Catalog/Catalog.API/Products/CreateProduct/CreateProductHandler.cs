c57ce51f8fe73380f2d20a6beaaf21ecc5da596b
Li <820208235@qq.com>
2025-10-05 18:17:44 +0800

    修改CQRS抽象引用

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,28 +1,42 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
 /// <summary>
-/// 新增产品请求模型
+/// 新增产品命令
 /// </summary>
 public record CreateProductCommand(string Name,
                                    List<string> Category,
                                    string Description,
                                    string ImageFile,
-                                   decimal Price) : IRequest<CreateProductResult>;
+                                   decimal Price) : ICommand<CreateProductResult>;
 
 /// <summary>
-/// 新增产品响应模型
+/// 新增产品响应
 /// </summary>
 public record CreateProductResult(Guid Id);
 
 /// <summary>
 /// 应用程序逻辑层
 /// </summary>
-public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        // 从命令对象创建产品实体
+        Product product = new()
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        // 将产品实体保存到数据库
+
+        // 返回创建的产品ID
+        return Task.FromResult(new CreateProductResult(product.Id));
     }
 }

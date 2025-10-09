8d5ac24608de8bb7d54fd59be1b9960518b13ad9
Li <820208235@qq.com>
2025-10-09 15:41:55 +0800

    新增删除产品

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -1,5 +1,19 @@
 ﻿namespace Catalog.API.Products.DeleteProduct;
 
-public class DeleteProductCommandHandler
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
 {
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("{ClassName}.{MethodName} call with {Command}", GetType().FullName, nameof(Handle), command);
+
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+
 }
--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,5 +1,28 @@
-﻿namespace Catalog.API.Products.DeleteProduct;
+﻿
+namespace Catalog.API.Products.DeleteProduct;
 
-public class DeleteProductEndpoint
+//public record DeleteProductRequest(Guid Id);
+
+public record DeleteProductResponse(bool IsSuccess);
+
+public class DeleteProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id:guid}", async (Guid id, ISender sender) =>
+        {
+            var command = new DeleteProductCommand(id);
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<DeleteProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteProduct")
+        .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Deletes a product by Id")
+        .WithDescription("Deletes a product by its unique identifier."); 
+    }
 }

bedf72315bce57a913c2338ec979ecb2adb20d41
Li <820208235@qq.com>
2025-10-16 15:34:11 +0800

    新增根据用户ID获取购物车

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,5 +1,24 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketEndpoint
+//public record GetBasketRequest(string UserName);
+
+public record GetBasketResponse(ShoppingCart Cart);
+
+public class GetBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var query = new GetBasketQuery(userName);
+            var result = await sender.Send(query);
+            var response = result.Adapt<GetBasketResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetBasket")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Gets the shopping cart for a user")
+        .WithDescription("Gets the shopping cart for a user by their id.");
+    }
 }
--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -1,5 +1,14 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketQueryHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        // TODO: 使用仓储从数据库获取购物车
+        return new GetBasketResult(new ShoppingCart(query.UserName));
+    }
 }

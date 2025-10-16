9fcb5376c5e44ecf34618471513e60ea54bf3d0d
Li <820208235@qq.com>
2025-10-16 16:18:23 +0800

    新增删除购物车

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketCommandHandler.cs
@@ -1,5 +1,22 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-internal class DeleteBasketCommandHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required.");
+    }
+}
+
+internal class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        // TODO: 从数据库和缓存中删除用户的购物车
+        return new DeleteBasketResult(true);
+    }
 }
--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,5 +1,24 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketEndpoint
+//public record DeleteBasketRequest(string UserName);
+
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var command = new DeleteBasketCommand(userName);
+            var result = await sender.Send(command);
+            return Results.Ok(new DeleteBasketResponse(result.IsSuccess));
+        })
+        .WithName("DeleteBasket")
+        .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Deletes a basket by user id.")
+        .WithDescription("Deletes a basket by its user id.");
+    }
 }

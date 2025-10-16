866fd44b2bdcd1dcdde15461c99a7b57c16594e6
Li <820208235@qq.com>
2025-10-16 17:22:24 +0800

    初步配置Program

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,33 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
 
+var builder = WebApplication.CreateBuilder(args);
 // 向容器中添加服务。
+// 一个为ASP.NET Core最小API提供拓展功能和模块化的轻量级框架
+// Carter包若安装在其他引用项目中则无法识别继承了ICarterModule的API
+builder.Services.AddCarter();
+// 注册MediatR中介类库
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    // 将此项目中的所有服务都注册到中介类库中
+    config.RegisterServicesFromAssembly(assembly);
+    // 将验证行为添加到MediatR的管道中
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    // 将日志行为添加到MediatR的管道中
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+// 注册FluentValidation验证器
+builder.Services.AddValidatorsFromAssembly(assembly);
+// 注册全局异常处理
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
 var app = builder.Build();
-
-
 // 配置HTTP请求管道。
+// 映射Carter模块中的路由
+app.MapCarter();
+// 使用全局异常处理中间件
+// 此处因BUG，必须传入一个空的options委托，否则生成时会抛出异常
+app.UseExceptionHandler(options => { });
 
 app.Run();

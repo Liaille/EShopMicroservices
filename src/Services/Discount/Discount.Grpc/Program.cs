7cd9e837fda1e348a0e5d13a6b5be2208d65ee88
Li <820208235@qq.com>
2025-10-22 23:11:00 +0800

    配置折扣服务使用Sqlite数据库

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,9 +1,13 @@
-﻿using Discount.Grpc.Services;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+// 注册DiscountDbContext并配置为使用SQLite数据库
+builder.Services.AddDbContext<DiscountDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 

36802bf501c151e09ba850e7d765080532d7e6dc
Li <820208235@qq.com>
2025-10-22 22:39:36 +0800

    初始化gRpc类型折扣项目

---
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace Discount.Grpc.Models;
+
+/// <summary>
+/// 优惠券
+/// </summary>
+public class Coupon
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public int Amount { get; set; }
+}

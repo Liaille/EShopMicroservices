7276c49be6c3c05a3ee6fd6cd69bdb0766c73d5e
Li <820208235@qq.com>
2026-03-18 22:27:17 +0800

    订单应用层新增订单模块数据传输对象

---
+++ src/Services/Order/Order.Application/Dtos/Order/AddressDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Order.Application.Dtos.Order;
+
+/// <summary>
+/// 订单地址DTO
+/// </summary>
+/// <param name="FirstName">姓</param>
+/// <param name="LastName">名</param>
+/// <param name="Email">邮箱</param>
+/// <param name="AddressLine">收件地址</param>
+/// <param name="Country">国家</param>
+/// <param name="State">省/州</param>
+/// <param name="ZipCode">邮政编码</param>
+[Serializable]
+public record AddressDto(
+    string FirstName,
+    string LastName,
+    string Email,
+    string AddressLine,
+    string Country,
+    string State,
+    string ZipCode);
---
+++ src/Services/Order/Order.Application/Dtos/Order/OrderItemDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Order.Application.Dtos.Order;
+
+/// <summary>
+/// 订单项DTO
+/// </summary>
+/// <param name="OrderId">订单ID</param>
+/// <param name="ProductId">产品Id</param>
+/// <param name="Quantity">数量</param>
+/// <param name="Price">价格</param>
+[Serializable]
+public record OrderItemDto(
+    Guid OrderId,
+    Guid ProductId,
+    int Quantity,
+    decimal Price);

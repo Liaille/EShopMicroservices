f152ff8b27b0a91e7d8544784c4845bbb0d57a62
Li <820208235@qq.com>
2026-03-27 21:03:02 +0800

    新增事件总线类库

---
+++ src/BuildingBlocks/BuildingBlocks.EventBus/Events/IntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace BuildingBlocks.EventBus.Events;
+
+public record IntegrationEvent
+{
+    public Guid Id { get; init; }
+
+    public DateTime OccurredOn { get; init; }
+
+    public string EventType => GetType().FullName!;
+
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        OccurredOn = DateTime.UtcNow;
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.EventBus/MassTransit/EventBusOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.EventBus.MassTransit;
+
+public class EventBusOptions
+{
+    public string Host { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
